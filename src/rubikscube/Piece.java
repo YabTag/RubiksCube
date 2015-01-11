ec2071fc0f04dc8b2d16572fb9866097cae03bc1
Yabtag <youssef949@hotmail.com>
2015-01-11 06:02:19 +0100

    Il manque la fonction turnFace

--- src/rubikscube/Piece.java
+++ src/rubikscube/Piece.java
@@ -5,26 +5,63 @@ import java.awt.Color;
 
 
 public class Piece {
-	public final static Color[] COULEURS ={Color.RED,Color.ORANGE,Color.BLUE,Color.GREEN,Color.YELLOW,Color.WHITE};
+	public final static Color[] COULEURS ={Color.RED,Color.ORANGE,Color.BLUE,Color.GREEN,Color.WHITE,Color.YELLOW};
 	
-	
-	
-
-}
-
-//class Interieur extends Piece{
+//	private int Up;
 //	
-//}
-
-
-class Centre extends Piece{
-	private Color couleur;
+//	private int Down;
+//	
+//	private int Right;
+//	
+//	private int Left;
+//	
+//	private int Front;
+//	
+//	private int Back;
 	
-	public Centre(int n){
-		this.couleur=COULEURS[n];
+	//tableau à 6 entrées représentant les orientation d'une piece de RubiksCube {Up,Down,Right,Left,Back,Front}
+	private int[] Orientation = new int[6];
+	
+	public Piece(int Up, int Down, int Right, int Left, int Back, int Front){
+		Orientation[0]=Up;
+		Orientation[1]=Down;
+		Orientation[2]=Right;
+		Orientation[3]=Left;
+		Orientation[4]=Back;
+		Orientation[5]=Front;
+		
 	}
-	public Color getCouleurCentre(){
-		return couleur;
+	
+	public boolean equals(Object piece){
+		if(piece==null){
+			return false;
+		}else if( piece.getClass() != getClass()){
+			return false;
+		}else{
+			Piece p=(Piece) piece;
+			for(int i=0; i<6; ++i){
+				if( (Orientation[i] ==-1 && !p.getColor(i).equals(null) ) || !COULEURS[Orientation[i]].equals(p.getColor(i)) ) {
+					return false;
+				}
+			}
+			return true;
+		}
 	}
+	
+	public Color getColor(int i){
+		int o= Orientation[i];
+		if(o==-1){
+			return null;
+		}else{
+			return COULEURS[o];
+		}
+	}
+	
+	public void permuteColor(int i,int j){
+		int aux=Orientation[i];
+		Orientation[i]=Orientation[j];
+		Orientation[j]=aux;
+	}
+	
+	
 }
-
--- src/rubikscube/Rubiks.java
+++ src/rubikscube/Rubiks.java
@@ -9,42 +9,103 @@ public class Rubiks {
 	
 	public void setRubiks(){
 		//Interieur*1
-		matrice[0][0][0]= new Piece();
+		matrice[0][0][0]= new Piece(-1,-1,-1,-1,-1,-1);
 		//6 Centres qui restent invariables
-		matrice[1][0][0]= new Centre(0);
-		matrice[2][0][0]= new Centre(1);
-		matrice[0][1][0]= new Centre(2);
-		matrice[0][2][0]= new Centre(3);
-		matrice[0][0][1]= new Centre(4);
-		matrice[0][0][2]= new Centre(5);
+		matrice[1][0][0]= new Piece(0,-1,-1,-1,-1,-1);
+		matrice[2][0][0]= new Piece(-1,1,-1,-1,-1,-1);
+		matrice[0][1][0]= new Piece(-1,-1,2,-1,-1,-1);
+		matrice[0][2][0]= new Piece(-1,-1,-1,3,-1,-1);
+		matrice[0][0][1]= new Piece(-1,-1,-1,-1,4,-1);
+		matrice[0][0][2]= new Piece(-1,-1,-1,-1,-1,5);
 		// Arete * 12
-		matrice[1][1][0]= new Arete(0,2);
-		matrice[1][2][0]= new Arete(0,3);
-		matrice[2][1][0]= new Arete(1,2);
-		matrice[2][2][0]= new Arete(1,3);
-		matrice[0][1][1]= new Arete(2,4);
-		matrice[0][1][2]= new Arete(2,5);
-		matrice[0][2][1]= new Arete(3,4);
-		matrice[0][2][2]= new Arete(3,5);
-		matrice[1][0][1]= new Arete(0,4);
-		matrice[1][0][2]= new Arete(0,5);
-		matrice[2][0][1]= new Arete(1,4);
-		matrice[2][0][2]= new Arete(1,5);
+		matrice[1][1][0]= new Piece(0,-1,2,-1,-1,-1);
+		matrice[1][2][0]= new Piece(0,-1,-1,3,-1,-1);
+		matrice[2][1][0]= new Piece(-1,1,2,-1,-1,-1);
+		matrice[2][2][0]= new Piece(-1,1,-1,3,-1,-1);
+		matrice[0][1][1]= new Piece(-1,-1,2,-1,4,-1);
+		matrice[0][1][2]= new Piece(-1,-1,2,-1,-1,5);
+		matrice[0][2][1]= new Piece(-1,-1,-1,3,4,-1);
+		matrice[0][2][2]= new Piece(-1,-1,-1,3,-1,5);
+		matrice[1][0][1]= new Piece(0,-1,-1,-1,4,-1);
+		matrice[1][0][2]= new Piece(0,-1,-1,-1,-1,5);
+		matrice[2][0][1]= new Piece(-1,1,-1,-1,4,-1);
+		matrice[2][0][2]= new Piece(-1,1,-1,-1,-1,5);
 		// Coin * 8
-		matrice[1][1][1]= new Coin(0,2,4);
-		matrice[1][1][2]= new Coin(0,2,5);
-		matrice[1][2][1]= new Coin(0,3,4);
-		matrice[1][2][2]= new Coin(0,3,5);
-		matrice[2][1][1]= new Coin(1,2,4);
-		matrice[2][1][2]= new Coin(1,2,5);
-		matrice[2][2][1]= new Coin(1,3,4);
-		matrice[2][2][2]= new Coin(1,3,5);
+		matrice[1][1][1]= new Piece(0,-1,2,-1,4,-1);
+		matrice[1][1][2]= new Piece(0,-1,2,-1,-1,5);
+		matrice[1][2][1]= new Piece(0,-1,-1,3,4,-1);
+		matrice[1][2][2]= new Piece(0,-1,-1,3,-1,5);
+		matrice[2][1][1]= new Piece(-1,1,2,-1,4,-1);
+		matrice[2][1][2]= new Piece(-1,1,2,-1,-1,5);
+		matrice[2][2][1]= new Piece(-1,1,-1,3,4,-1);
+		matrice[2][2][2]= new Piece(-1,1,-1,3,-1,5);
 	}
+	
+	
+	public Rubiks(){
+		setRubiks();
+	}
+	
 	
 	public Piece[][][] getRubiks(){
 		return matrice;
 	}
 	
+	public Piece[][] getFace(int i){
+		int p= i-i%2;
+		Piece[][] face=new Piece[3][3];
+		for(int j=0;j<3;++j){
+			for(int k=0;k<3;++k){
+				if(p==0){
+					face[j][k]=matrice[i%2+1][j][k];
+				}else if(p==2){
+					face[j][k]=matrice[k][i%2+1][j];
+				}else{
+					face[j][k]=matrice[j][k][i%2+1];
+				}
+			}
+		}
+		
+		return face;
+		
+		
+	}
 	
+	public void turnFace(int i,int degree){
+		
+		if(degree==1){
+			
+			int p = i-i%2; // prendre le nombre pair le plus proche de i inferieurement.
+			//définir les axes pour les quels la couleur doit permutter
+			int axe1= (p+2)%6 ;
+			int axe2= (p+4)%6 ;
+			
+			
+			
+			
+			
+			
+//			for(int k=0; k<2 ;++k ){
+//				for(int l=0; l<2; ++l){
+//					for(int m=0; m<2; ++m){
+//						if(matrice[k][l][m].getColor(i) != null){
+//							Piece piece=matrice[k][l][m];
+//							piece.permuteColor(axe1,axe2);
+//							piece.permuteColor(axe2,axe1 + 1);
+//							piece.permuteColor(axe1 +1 , axe2 + 1);
+//						}
+//					}
+//				}
+//			}
+		}else if(degree==2){
+			turnFace(i,1);
+			turnFace(i,1);
+		}else if(degree==3){
+			turnFace(i,2);
+			turnFace(i,1);
+		}
+		
+		
+	}
 	
 }
